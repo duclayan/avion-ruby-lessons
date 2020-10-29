d3eef16ea38f1df855d6c3cf1d7bb934d25f8c7e
duclayan <duclayandominique@gmail.com>
2020-10-29 15:39:48 +0400

    day 3: added comments

--- lesson-3-own-class.rb
+++ lesson-3-own-class.rb
@@ -6,10 +6,12 @@ class Student
     end
 
     def introduce
-        puts "My name is #{@name} and I am #{@age} years old"
+        puts "My name is #{@name} and I am #{@age} years old. My class role number is #{@rolenumber}"
     end
 end
 
 stud1 = Student.new("Ariel", "19", "S23")
 
 stud1.introduce
+
+# The class contains the attributes name,age and rolenumber. It also contains a method:introduce to introduce themselves.
