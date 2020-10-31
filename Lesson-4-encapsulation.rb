58f6d6fdd4273525b20327bea346e72b3380d8ca
duclayan <duclayandominique@gmail.com>
2020-10-31 07:47:53 +0400

    Changed to snake case

--- Lesson-4-encapsulation.rb
+++ Lesson-4-encapsulation.rb
@@ -1,4 +1,4 @@
-class YourPhone
+class Your_phone
     attr_accessor :passcode
     def initialize(owner,passcode,email)
         @owner = owner
@@ -6,20 +6,20 @@ class YourPhone
         @email = email
     end
 
-    def phoneUser
+    def phone_user
         puts "Phone User: #{@owner}"
         puts "Passcode: #{@passcode}"
         puts "Email: #{@email}"
     end
 
     
-    def randomizePassword
+    def randomize_password
         @passcode = @passcode * 4
     end
 end
 
-user1 = YourPhone.new("Kazz", 921, "kazz@gmail.com")
-user2 = YourPhone.new("Kali",313,"Kalies@poasd.com")
+user1 = Your_phone.new("Kazz", 921, "kazz@gmail.com")
+user2 = Your_phone.new("Kali",313,"Kalies@poasd.com")
 
-user1.randomizePassword()
+user1.randomize_password()
 puts user1.passcode
