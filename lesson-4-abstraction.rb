b5c7349b28b601a05481d815f16b666d062cfe13
duclayan <duclayandominique@gmail.com>
2020-10-31 08:17:30 +0400

    Lesson 4

--- lesson-4-abstraction.rb
+++ lesson-4-abstraction.rb
@@ -1,17 +1,20 @@
 class Bank_account
     attr_accessor :balance
+    attr_accessor :add_to_record
     def initialize(account_number,balance)
         @account_number = account_number
         @balance = balance
-        add_to_record = []
+        @add_to_record = []
     end
 
     def withdraw(amount)
         @balance -= amount.to_i
+        @add_to_record.push(" #{amount} withdrawed")
     end
 
     def deposit(amount)
         @balance += amount.to_i
+        @add_to_record.push("#{amount} deposited")
     end
 end
 
@@ -20,4 +23,4 @@ account1.withdraw(231)
 account1.withdraw(2343)
 account1.withdraw(2321)
 account1.deposit(2424)
-puts account1.balance
+puts account1.add_to_record
