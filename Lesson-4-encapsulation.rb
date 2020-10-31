12b9466ca30765bdc7d3251522eea4178e3162f1
duclayan <duclayandominique@gmail.com>
2020-10-31 07:44:58 +0400

    encapsulation

--- Lesson-4-encapsulation.rb
+++ Lesson-4-encapsulation.rb
@@ -1,4 +1,5 @@
 class YourPhone
+    attr_accessor :passcode
     def initialize(owner,passcode,email)
         @owner = owner
         @passcode = passcode
@@ -10,10 +11,15 @@ class YourPhone
         puts "Passcode: #{@passcode}"
         puts "Email: #{@email}"
     end
+
+    
+    def randomizePassword
+        @passcode = @passcode * 4
+    end
 end
 
-user1 = YourPhone.new("Kazz", "9213", "kazz@gmail.com")
-user2 = YourPhone.new("Kali","313","Kalies@poasd.com")
+user1 = YourPhone.new("Kazz", 921, "kazz@gmail.com")
+user2 = YourPhone.new("Kali",313,"Kalies@poasd.com")
 
-user1.phoneUser()
-user2.phoneUser
+user1.randomizePassword()
+puts user1.passcode
