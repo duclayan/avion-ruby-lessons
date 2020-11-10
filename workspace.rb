66b71b8faf6d6964a4b83e7397db18239fe0c8f2
duclayan <duclayandominique@gmail.com>
2020-11-10 15:32:02 +0400

    Code Challenge 2

--- workspace.rb
+++ workspace.rb
@@ -1,15 +1,9 @@
 array_one = [34,15,88,2]
 array_two = [34,-345,-1,100]
 def find_minimum(x)
-  min = x[0]
-    x.each do |ctr|
-      if min > ctr
-        min = ctr
-      end
-    end
-    
-    return min
+    min = x[0]
+    x.each { |num| min=num if min>num }
+    min
 end
-
 puts find_minimum(array_one)
 puts find_minimum(array_two)
