187068f9010a664bd9bc80393b9c88c7529a4325
duclayan <duclayandominique@gmail.com>
2020-11-16 17:43:50 +0400

    Coding Challenge 3 Corrections

--- code-challenge/code-challenge-3.rb
+++ code-challenge/code-challenge-3.rb
@@ -1,10 +1,17 @@
 array_one = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]
 
 def getSum(x)
+
+    return [] unless x.length > 0
+
     total = [0,0]
+
     x.each do |num|
         num = num.to_i
-        (num > 0) ? (total[0] += 1) : total[1] += num
+        (num > 0) ? (total[0] += 1) : (total[1] += num)
     end
+    
     return total
 end
+
+puts getSum(array_one)
--- workspace.rb
+++ workspace.rb
@@ -1,11 +1,12 @@
-array_one = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15, 'b']
-
+array_one = [1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]
 def getSum(x)
+    return [] unless x.length > 0
     total = [0,0]
     x.each do |num|
-        (num > 0) ? (total[0] += 1) : total[1] += num
+        num = num.to_i
+        (num > 0) ? (total[0] += 1) : (total[1] += num)
     end
-    puts total
     return total
 end
-puts (getSum(array_one).is_a? Array)
+
+puts getSum(array_one)
